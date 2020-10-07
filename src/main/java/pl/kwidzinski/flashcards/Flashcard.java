28fe8b8759e2692d87950115a91c08e48cc32cb0
PawelKwidzinski <solvap83@gmail.com>
2020-10-07 09:38:44 +0200

    Methods 'ask()' & 'remove()' in Flashcards.java created

--- src/main/java/pl/kwidzinski/flashcards/Flashcard.java
+++ src/main/java/pl/kwidzinski/flashcards/Flashcard.java
@@ -19,7 +19,7 @@ public class Flashcard {
         Flashcard.log.add("The card:");
         String term = scanner.nextLine();
         Flashcard.log.add(term);
-        if (flashcards.containsKey(term)){
+        if (flashcards.containsKey(term)) {
             System.out.printf("The card \"%s\" already exists.\n", term);
             Flashcard.log.add("The card \"" + term + "\" already exists.");
             return;
@@ -29,15 +29,69 @@ public class Flashcard {
         Flashcard.log.add("The definition of the card:");
         String definition = scanner.nextLine();
         Flashcard.log.add(definition);
-        if (flashcards.containsValue(definition)){
+        if (flashcards.containsValue(definition)) {
             System.out.printf("The definition \"%s\" already exists.\n", definition);
             Flashcard.log.add("The definition \"" + definition + "\" already exists.");
             return;
         }
 
         flashcards.put(term, definition);
-        mistakes.put(term,0);
+        mistakes.put(term, 0);
         System.out.printf("The pair (\"%s\":\"%s\") has been added.\n", term, definition);
         Flashcard.log.add("The pair (\"" + term + "\":\"" + definition + "\") has been added.\n");
+    }
+
+    public void ask() {
+        System.out.println("How many times to ask?");
+        Flashcard.log.add("How many times to ask?");
+        int n = Integer.parseInt(scanner.nextLine());
+        Flashcard.log.add(String.valueOf(n));
+        String[] terms = new String[flashcards.keySet().size()];
+        flashcards.keySet().toArray(terms);
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            String term = terms[random.nextInt(terms.length)];
+
+            System.out.printf("Print the definition of \"%s\":\n", term);
+            Flashcard.log.add("Print the definition of \"" + term + "\":");
+            String input = scanner.nextLine();
+            Flashcard.log.add(input);
+
+            if (input.equals(flashcards.get(term))) {
+                System.out.println("Correct answer.");
+                Flashcard.log.add("Correct answer.");
+            } else if (flashcards.containsValue(input)) {
+                String otherTerm = "";
+                for (String s : flashcards.keySet()) {
+                    if (flashcards.get(s).equals(input)) {
+                        otherTerm = s;
+                    }
+                }
+                System.out.printf("Wrong answer. The correct one is \"%s\",", flashcards.get(term));
+                System.out.printf("you've just written the definition of \"%s\".\n", otherTerm);
+                Flashcard.log.add("Wrong answer. The correct one is \"" + flashcards.get(term) +
+                        "\",you've just written the definition of \"" + otherTerm + "\".");
+                mistakes.replace(term, mistakes.get(term) + 1);
+            } else {
+                System.out.printf("Wrong answer. The correct one is \"%s\".\n", flashcards.get(term));
+                Flashcard.log.add("Wrong answer. The correct one is \"" + flashcards.get(term) + "\".");
+                mistakes.replace(term, mistakes.get(term) + 1);
+            }
+        }
+    }
+
+    public void remove() {
+        System.out.println("The card:");
+        Flashcard.log.add("The card:");
+        String term = scanner.nextLine();
+        if (flashcards.containsKey(term)) {
+            flashcards.remove(term);
+            mistakes.remove(term);
+            System.out.println("The card has been removed.");
+            Flashcard.log.add("The card has been removed.");
+        } else {
+            System.out.printf("Can't remove \"%s\": there is no such card.", term);
+            Flashcard.log.add("Can't remove \"" + term + "\": there is no such card.");
+        }
     }
 }
